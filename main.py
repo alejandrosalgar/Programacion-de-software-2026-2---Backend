f1e343f729935a7c800ac2229ff66137a178c6ad
alejandrosalgar <alejandro.salgar@gmail.com>
2026-08-14 19:11:18 -0500

    Feat: agregar descripcion de funcion suma

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def sumar(a: int, b: int) -> int:
+    """
+    Suma dos numeros
+    Args:
+        a: Primer numero
+        b: Segundo numero
+    Returns:
+        int: La suma de los dos numeros
+
+    """
     return a + b
 
 
 print(sumar(2, 6))
-# jfhaksjdfhlkasjdfhkasjdfhlk
